63fc62218344a9437f7e0f7890ca462f9a322540
Pedroska21 <nero1928c@gmail.com>
2026-05-29 12:52:46 -0300

    Delete courses directory

--- courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from courses.models import Course, Category, Episodes
-
-admin.site.register(Course)
-admin.site.register(Category)
-admin.site.register(Episodes)
-# Register your models here.
--- courses/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import course
-
-
-class CourseForm(ModelForm):
-    class Meta:
-        model = course
-        fields = ['name', 'hours', 'description', 'banner', 'category', 'episode']
