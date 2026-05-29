c253bdbe71d909457e1fa76b88b9da2202886234
Pedroska21 <nero1928c@gmail.com>
2026-05-29 12:53:21 -0300

    Delete users directory

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import CourseLoginView, Resgister
-
-urlpatterns = [
-    path('', CourseLoginView.as_view(), name='login'),
-    path ('registrar/', Resgister.as_view(), name='register'),
-]
